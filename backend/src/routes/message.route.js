dd4b27c96c8071a1e80e6361d292afcdeee3ddcc
RSantiago <RSantiago@UBVPNN11431.core.unionbank.com>
2025-08-01 02:53:48 +0800

    commit lesson 1:30m

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { protectedRoute } from '../middleware/auth.middleware.js';
-import { getUserForSidebar } from '../controllers/message.controller.js';
+import { getMessages, getUserForSidebar } from '../controllers/message.controller.js';
 const router = express.Router();
 
 
-router.get('user', protectedRoute, getUserForSidebar)
+router.get('/user', protectedRoute, getUserForSidebar);
+router.get('/:id', protectedRoute, getMessages);
 
-
+router.post('/send/:id', protectedRoute, sendMessage);
 export default router;

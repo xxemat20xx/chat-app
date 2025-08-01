ee7f223d68a0fe52a43f0236530d2d657a405443
xxemat20xx <62041802+xxemat20xx@users.noreply.github.com>
2025-08-01 11:01:05 +0800

    commit 1:40

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { protectedRoute } from '../middleware/auth.middleware.js';
-import { getMessages, getUserForSidebar } from '../controllers/message.controller.js';
+import { getMessages, getUserForSidebar, sendMessages } from '../controllers/message.controller.js';
 const router = express.Router();
 
 
 router.get('/user', protectedRoute, getUserForSidebar);
 router.get('/:id', protectedRoute, getMessages);
 
-router.post('/send/:id', protectedRoute, sendMessage);
+router.post('/send/:id', protectedRoute, sendMessages);
 export default router;

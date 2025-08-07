542a91dee540f2974d15a353527d4add175f86e0
xxemat20xx <62041802+xxemat20xx@users.noreply.github.com>
2025-08-08 07:35:53 +0800

    commit lesson 3:26

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -3,10 +3,11 @@ import { useChatStore } from "../store/useChatStore"
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
 import MessageSkeleton from "./Skeletons/MessageSkeleton";
+import { useAuthStore } from "../store/useAuthStore";
 
 const ChatContainer = () => {
   const { messages, getMessages, isMessagesLoading, selectedUser} = useChatStore();
-  
+  const { authUser } = useAuthStore();
   useEffect(() => {
       getMessages(selectedUser._id);
   }, [selectedUser._id, getMessages])
@@ -23,7 +24,34 @@ const ChatContainer = () => {
     <div className="flex flex-1 flex-col overflow-auto">
         <ChatHeader />
 
-        <p>messages...</p>
+        {/* Chat inputs */}
+        <div className="flex flex-1 flex-col overflow-auto p-4 space-y-4">
+            {messages.map((message) => (
+              <div key={message._id}
+              className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}>
+                  <div className="chat-image avatar">
+                    <div className="size-10 rounded-full border">
+                        <img src={message.senderId === authUser._id ? authUser.profilePic || "/avatar.png" : selectedUser.profilePic || "/avatar.png"} 
+                        alt="profile pic" />
+                    </div>
+                  </div>
+                  <div className="chat-header mb-1">
+                    <time className="text-xs opacity-50 ml-1">{message.createdAt}</time>
+                  </div>
+                  <div className="chat-bubble flex">
+                      {message.image && (
+                        <img 
+                          src={message.image}
+                          alt="attachment"
+                          className="sm:max-w-[200px] rounded-md mb-2"
+
+                        />
+                      )}
+                      {message.text && <p>{message.text}</p>}
+                  </div>
+              </div>
+            ))}
+        </div>
 
         <MessageInput />
     </div>
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -25,7 +25,7 @@ export const useChatStore = create((set, get) => ({
         set({isMessagesLoading: true});
         try {
             const res = await axiosInstance.get(`/messages/${userId}`);
-            set({ message: res.data});
+            set({ messages: res.data});
         } catch (error) {
              toast.error(error.response.data.message);
         }finally{

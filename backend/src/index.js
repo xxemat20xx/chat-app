f4eaba703cc68e2bc51a72a80bf623a652e3fae5
xxemat20xx <62041802+xxemat20xx@users.noreply.github.com>
2025-08-14 08:49:27 +0800

    fix to deployment issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,9 +30,10 @@ app.use('/api/messages', messageRoutes);
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
     });
+
 }
 
 server.listen(PORT, () => {
